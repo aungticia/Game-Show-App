a8263fe05c4282b70297f2eeaab203ce080a0646
Aung <104800264+aungoo3@users.noreply.github.com>
2023-10-28 17:29:27 -0400

    Updated Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,4 +4,55 @@
 
 class Phrase  {
 
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+
+    };
+    addPhraseToDisplay() {
+        const phraseContainer = document.querySelector('#phrase ul');
+        const phraseSplit = this.phrase.split('');
+    
+        let html = '';
+    
+        for (let i = 0; i < phraseSplit.length; i++) {
+            if (phraseSplit[i] === ' ') {
+                html += `
+                    <li class='space'></li>
+                `;
+            } else {
+                html += `
+                    <li class='letter'>${phraseSplit[i]}</li>
+                `;
+            }
+        }
+    
+        phraseContainer.insertAdjacentHTML('beforeend', html);
+    }    
+
+    checkLetter(letter) {
+        return this.phrase.includes(letter);
+    };
+
+    showMatchedLetter(letter) {
+        const letterElements = document.querySelectorAll('#phrase ul li');
+        
+        for (let i = 0; i < letterElements.length; i++) {
+            if (letterElements[i].textContent === letter) {
+                letterElements[i].classList.remove('hide');
+                letterElements[i].classList.add('show');
+            }
+        }
+    }    
+     
 };
+
+
+
+
+
+
+
+
+
+
+
