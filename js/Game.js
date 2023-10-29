bfc6322f2c60473f7ac85e5d2cb46536cd83fc34
Aung <104800264+aungoo3@users.noreply.github.com>
2023-10-29 00:03:07 -0400

    Updated all the classes

--- js/Game.js
+++ js/Game.js
@@ -3,5 +3,94 @@
  * Game.js */
 
 class Game {
-
-};
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+          new Phrase('You Win'),
+          new Phrase('See You'),
+          new Phrase('Not Today'),
+          new Phrase('I Love You'),
+          new Phrase('Say Something')
+        ];
+        this.activePhrase = null;
+      }
+    
+      getRandomPhrase() {
+        const randomIndex = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomIndex];
+      }
+    
+      startGame() {
+        const overlay = document.getElementById('overlay');
+        overlay.style.display = 'none';
+    
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+      }
+  
+      handleInteraction(button) {
+        button.disabled = true;
+        const letter = button.textContent;
+      
+        if (this.activePhrase.checkLetter(letter)) {
+          button.classList.add('chosen');
+          this.activePhrase.showMatchedLetter(letter);
+          if (this.checkForWin()) {
+            this.gameOver(true);
+          }
+        } else {
+          button.classList.add('wrong');
+          this.removeLife();
+        }
+      }      
+  
+    removeLife() {
+      const hearts = document.querySelectorAll('.tries img');
+      hearts[this.missed].src = 'images/lostHeart.png';
+      this.missed++;
+      if (this.missed === 5) {
+        this.gameOver(false);
+      }
+    }
+  
+    checkForWin() {
+        const letters = document.querySelectorAll('.letter');
+        return [...letters].every(letter => letter.classList.contains('show'));
+      }      
+      
+  
+    gameOver(isWin) {
+      const overlay = document.getElementById('overlay');
+      overlay.style.display = 'flex';
+      const message = document.getElementById('game-over-message');
+      overlay.classList.remove('start', 'win', 'lose');
+      overlay.classList.add(isWin ? 'win' : 'lose');
+      message.textContent = isWin ? 'Congratulations! You won!' : 'Sorry, you lost. Try again!';
+      this.resetGame();
+    }
+  
+    resetGame() {
+        // Remove all li elements from the Phrase ul element
+        const phraseList = document.querySelector('#phrase ul');
+        phraseList.innerHTML = '';
+    
+        // Enable all of the onscreen keyboard buttons and update their classes
+        const keys = document.querySelectorAll('.key');
+        keys.forEach((key) => {
+          key.classList.remove('chosen', 'wrong');
+          key.classList.add('key');
+          key.disabled = false;
+        });
+    
+        // Reset all of the heart images in the scoreboard
+        const hearts = document.querySelectorAll('.tries img');
+        hearts.forEach((heart) => {
+          heart.src = 'images/liveHeart.png';
+        });
+    
+        // Reset missed count and activePhrase
+        this.missed = 0;
+        this.activePhrase = null;
+      }
+  }
+  
--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const game = new Game();
+    
+    // Function to handle keyboard input
+    const handleKeyboardInput = (event) => {
+      if (/^[a-zA-Z]$/.test(event.key) && !event.target.disabled) {
+        const letter = event.key.toLowerCase();
+        const buttons = document.querySelectorAll('.key');
+        const button = Array.from(buttons).find((btn) => btn.textContent === letter);
+  
+        if (button) {
+          game.handleInteraction(button);
+        }
+      }
+    };
+  
+    // Event listener for the "Start Game" button
+    const startButton = document.getElementById('btn__reset');
+    startButton.addEventListener('click', () => {
+      game.resetGame(); // Reset the game state if it's a new game
+      game.startGame();
+    });
+  
+    // Event delegation for the onscreen keyboard buttons
+    const qwerty = document.getElementById('qwerty');
+    qwerty.addEventListener('click', (event) => {
+      if (event.target.tagName === 'BUTTON') {
+        game.handleInteraction(event.target);
+      }
+    });
+  
+    // Event listener for keyboard input
+    document.addEventListener('keydown', handleKeyboardInput);
+  });
+  
